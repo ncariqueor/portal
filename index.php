0173585c9af83146a23e48459ed46b544de9c2a0
ncariqueor <ncariqueor@yahoo.cl>
2016-12-19 09:59:09 -0300

    Portal General de Reportería
    Este es el portal general que muestra todos los reportes online que
    existen actualmente.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Reportería Online</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap.css"/>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-88784345-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+    </head>
+
+    <body>
+        <h1 class="text-center"><img src="paris.png" width="140px" height="100px"> Reportes Online</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Ventas</div>
+                        <div class="panel-body">
+                            <a href="http://10.95.17.114/ventas_txd">Ventas Tiendas por Departamento</a><br>
+                            <a href="http://10.95.17.114/cump_venta">Seguimiento de Metas</a><br>
+                            <a href="http://10.95.17.114/ventas/perfomanceinternet.php">Performance Paris Internet</a><br>
+                            <a href="http://10.95.17.114/ventahora/venta">Venta por hora Paris Internet</a><br>
+                            <a href="http://10.95.17.114/clickcollect/clickandcollect.php">Click & Collect</a><br>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">Stock</div>
+                        <div class="panel-body">
+                            <a href="http://10.95.17.114/mantigua/informe.php">Mercadería Antigua</a><br><br><br><br>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Operaciones</div>
+                        <div class="panel-body">
+                            <a href="http://10.95.17.114/validaciones/reporte.php">Performance Validación</a><br>
+                            <a href="http://10.95.17.114/mix_productos/">Informe de Mix de Productos</a><br><br><br>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <div class="panel panel-warning">
+                        <div class="panel-heading">Fotografía y Catálogo</div>
+                        <div class="panel-body">
+                            <a href="http://10.95.17.114/catalogo/inf_fot_cat.php">Informe de Fotografía</a><br>
+                            <a href="http://10.95.17.114/catalogo/catalogo.php">Informe de Catálogo</a><br><br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
